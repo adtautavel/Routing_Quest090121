41b1ebaca28e496d43eb7a4e73d70dcf3737b440
adtautavel <adtautavel@yahoo.fr>
2021-01-09 23:53:49 +0100

    Add project files.

---
+++ Routing_Quest090121/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routing_Quest090121.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello()
+        {
+            return "SayHello!!";
+        }
+
+    }
+}
